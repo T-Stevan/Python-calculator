cbdb21782f865a35a43b90c7f56ca070ad76f403
TSM <trevor.stevan@gmail.com>
2026-01-07 17:57:19 +0300

    Added an option menu

--- calculator.py
+++ calculator.py
@@ -1,26 +1,59 @@
 def add(a, b):
     result = a + b
-    print(result)
+    return result
     
 def subtract(a, b):
     result = a - b
-    print(result)
+    return result
     
 def multiply(a, b):
     result = a * b
-    print(result)
+    return result
 
 def divide(a, b):
     try:
         result = a / b
-        print(result)
+        return result
     except ZeroDivisionError:
         print("Can't divide by Zero")
     
 def power(a, b):
     result = a ** b
-    print(result)
+    return result
     
-add( 5, 7)
-power(3, 2)
-divide(4,0)
+while True:
+    print("----Menu: ----")
+    print("1: Addition")
+    print("2: Subtraction")
+    print("3: Multiplication")
+    print("4: Division")
+    print("5: Exponential")
+    print("6: Exit calculator")
+
+    choice = input(" Choose one option from (1-6)")
+    if choice == "6":
+        print("---Power off----") 
+        break
+    
+    if choice not in ["1", "2", "3", "4", "5", "6"]:
+        print("Invalid option, try again")
+        continue
+    
+    num1 = int(input("Enter first number: \n"))
+    num2 = int(input("Enter second number: \n"))
+    
+    if choice == "1":
+        print("Result: ", add(num1, num2) )
+    elif choice == "2":
+        print("Result: ", subtract(num1, num2))
+    elif choice == "3":
+        print("Result: ", multiply(num1, num2))
+    elif choice == "4":
+        print("Result: ", divide(num1, num2))
+    elif choice == "5":
+        print("Result: ", power(num1, num2))
+    elif choice == "6":
+        print("---Power off----") 
+        break
+    
+
